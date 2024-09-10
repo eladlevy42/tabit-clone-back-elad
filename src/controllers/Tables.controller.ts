59d68dcf708adf538650494e200ab870580d133a
Elad Levy <162992264+eladlevy42@users.noreply.github.com>
2024-09-10 10:28:26 +0300

    Elad try 5

--- src/controllers/Tables.controller.ts
+++ src/controllers/Tables.controller.ts
@@ -19,28 +19,20 @@ export async function getAllAvaliableTablesByRest(
   try {
     const pool = await connectDB();
     connection = await pool.getConnection();
-    // Call the stored procedure to get available tables nearby
+
+    // Directly use the `dd-mm-yyyyThh:mm` format without conversion
     const [rows]: [RowDataPacket[], any] = await connection.query(
       `CALL get_available_tables_by_rest(${restId});`
     );
+
     if (!rows || rows.length === 0) {
       res.status(404).json({ message: "No tables found." });
       return;
     }
+
     res.status(200).json(rows[0]);
   } catch (error: any) {
     console.error("Error fetching available tables:", error);
-
-    if (error.code === "ER_CON_COUNT_ERROR") {
-      console.error("Too many connections to the database.");
-    } else if (error.code === "ECONNREFUSED") {
-      console.error("Database connection was refused.");
-    } else if (error.code === "ETIMEDOUT") {
-      console.error("Connection to the database timed out.");
-    } else if (error.code === "ER_ACCESS_DENIED_ERROR") {
-      console.error("Access denied for the database user.");
-    }
-
     res.status(500).json({ message: "Server error", error: error.message });
   } finally {
     if (connection) connection.release();
